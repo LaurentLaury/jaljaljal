c8aa43fe43a3a4f1f9f2f6b9eb65797358566433
YeolDoo <kdy0802@naver.com>
2020-08-14 14:38:20 +0900

    추천 알고리즘 모듈화 파일 추가

---
+++ jjj_login.py
@@ -0,0 +1,7 @@
+# jjj_manage에 들어가는 로그인 함수
+# 고객 존재 여부 확인
+# def checking_name(inserted_name):
+#     if df["이름"].str.contains(inserted_name).any():
+#         return 1
+#     else:
+#         return 0
