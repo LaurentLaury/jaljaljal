c64f08b0d9e41aad7a991f013e8735c2f0ca9eb6
YeolDoo <kdy0802@naver.com>
2020-08-14 17:03:31 +0900

    추천 알고리즘 모듈화 파일 추가

--- jjj_final.py
+++ jjj_final.py
@@ -4,11 +4,9 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 import folium
-import recommend_hybrid as rh
-import recommend_svd as rs
-import get
 import main as mm
-import jjj_manage as jm
+import best_predictions as bp
+import jjj_login as jl
 
 
 app = Flask(__name__)
@@ -22,19 +20,46 @@ def home():
 @app.route("/post", methods=['POST'])
 def post():
     value = request.form['input']
-    result = jm.checking_name(value)
+    result = jl.checking_name(value)
     if (result == 1):
         return render_template("jjj/login_success.html", name=value)
     else:
         return render_template("jjj/login_fail.html", name=value)
 
 
-@app.route("/hybrid/<name>")
-def hybrid(name):
+@app.route("/rec/<name>")
+def rec(name):
     result = mm.main(name)
     result_1 = result.to_json(orient='records', force_ascii=False)
-    return render_template("jjj/hybrid.html", data=result_1)
+    result_2 = result_1.load_json()
+    return render_template("jjj/rec.html", data=result_2)
 
+# @app.route("/rec/<name>")
+# def rec(name):
+#     result = mm.main(name)
+#     result_1 = f"{{ from: {result['장소']}, to: {result['장소']}, {result['p']} }}"
+#
+#     return render_template("jjj/rec.html", data=result_1)
+
+
+@app.route("/rec/<name>/<area>")
+def rec_area(name, area):
+    result = mm.main(name, area)
+    result_1 = result.to_json(orient='records', force_ascii=False)
+    return render_template("jjj/rec_area.html", data=result_1)
+
+
+@app.route("/rec/<name>/<area>/<ctg>")
+def rec_ctg(name, area, ctg):
+    result = mm.main(name, area, ctg)
+    result_1 = result.to_json(orient='records', force_ascii=False)
+    return render_template("jjj/rec_ctg.html", data=result_1)
+
+@app.route("/best_pred")
+def best_pred():
+    result = bp.best_pred()
+    result_1 = result.to_json(orient='records', force_ascii=False)
+    return render_template("jjj/best_pred.html", data=result_1)
 
 
 
--- jjj_login.py
+++ jjj_login.py
@@ -1,7 +1,36 @@
-# jjj_manage에 들어가는 로그인 함수
-# 고객 존재 여부 확인
-# def checking_name(inserted_name):
+# # jjj_jl에 들어가는 로그인 함수
+# # 고객 존재 여부 확인
+# import os
+import cx_Oracle
+# os.putenv('NLS_LANG', 'KOREAN_KOREA.KO16MSWIN949')
+# oracle_dsn = oci.makedsn(host="192.168.2.27", port=1521, sid="xe")
+# conn = oci.connect(dsn=oracle_dsn, user="hr", password="hr")
+# connection = cx_Oracle.connect('hr/hr@192.168.2.27:1521/xe')
+# cur = connection.cursor()
+# cur.execute(
+#     'select name, place, category, star, address ,category1, address1, address2, count, latitude, longitude, year, month, season from recommend')
+# # def checking_name(inserted_name):
+#
 #     if df["이름"].str.contains(inserted_name).any():
 #         return 1
 #     else:
 #         return 0
+import os
+os.putenv('NLS_LANG', 'KOREAN_KOREA.KO16MSWIN949')
+
+connection = cx_Oracle.connect('hr/hr@192.168.2.27:1521/xe')
+cur = connection.cursor()
+cur.execute("select membername from member")
+
+member = []
+for result in cur:
+    member.append(result[0])
+# 커넥션 종료
+cur.close()
+connection.close()
+
+def checking_name(inserted_name):
+    if inserted_name in member :
+        return 1
+    else :
+        return 0
