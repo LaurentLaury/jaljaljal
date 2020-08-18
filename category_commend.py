dbbbd01578090fffe8dbc7836b5a13120e6493ef
Kangmoonmin <52037950+Kangmoonmin@users.noreply.github.com>
2020-08-18 16:33:45 +0900

    데이터베이스 conn.close() 추가

--- category_commend.py
+++ category_commend.py
@@ -7,6 +7,8 @@ def get_name(name):
     cursor = conn.cursor()
     cursor.execute(sql, {"name": name})
     result = cursor.fetchall()
+    cursor.close()
+    conn.close()
     return result
 
 
@@ -15,6 +17,8 @@ def get_address1():
     cursor = conn.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
+    cursor.close()
+    conn.close()
     return result
 
 
@@ -31,4 +35,6 @@ def get_category1():
     cursor = conn.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
+    cursor.close()
+    conn.close()
     return result
