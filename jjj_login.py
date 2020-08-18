f1f97b41ad90822793f4cbf6f4bcefa3df4bb45b
user4 <user4@iot.com>
2020-08-18 18:24:40 +0900

    login 변경

--- jjj_login.py
+++ jjj_login.py
@@ -10,22 +10,20 @@ os.putenv('NLS_LANG', 'KOREAN_KOREA.KO16MSWIN949')
 oracle_dsn = oci.makedsn(host="192.168.2.27", port=1521, sid="xe")
 conn = oci.connect(dsn=oracle_dsn, user="hr", password="hr")
 
-sql = "select name from recommend"
+name_list=[]
+sql = "select distinct name from recommend"
 cursor = conn.cursor()
 cursor.execute(sql)
-review = cursor.fetchall()
+
+for name in cursor :
+    name_list.append(name[0])
+
 cursor.close()
-review = pd.DataFrame(review)
-review.columns = ['이름']
-
-
-c=review
-df = pd.DataFrame()
-df["이름"] = c["이름"]
+conn.close()
 
 #고객 정보 확인
 def checking_name(inserted_name):
-    if df["이름"].str.contains(inserted_name).any():
+    if inserted_name in name_list:
         return 1
     else:
         return 0
